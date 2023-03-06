f4c57349426aeea94d6478eb37c20215cb6f2502
shubhangikamble1 <121850846+shubhangikamble1@users.noreply.github.com>
2023-03-06 15:35:14 +0530

    Update Demo2Application.java

--- src/main/java/com/yash/ems/Demo2Application.java
+++ src/main/java/com/yash/ems/Demo2Application.java
@@ -6,10 +6,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
-public class Demo2Application {
+public class EMSApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Demo2Application.class, args);
+		SpringApplication.run(EMSApplication.class, args);
 		System.out.println("EMS Application started...!!!");
 		
 	}
